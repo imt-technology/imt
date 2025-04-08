e42fb983badf394a734cb35a5b9ee41761b0c569
vekidev <93841364+vekidev@users.noreply.github.com>
2025-04-08 22:39:46 +0200

    Update main.py

--- rpi/main.py
+++ rpi/main.py
@@ -1,7 +1,17 @@
 import cameralib
 import commlib
 import sensorlib
+from servercomm import *
 
 if __name__ == "__main__":
     arduino = commlib.ArduinoComm()
-    cameralib.get_cv2_stream()
+    cam1 = cameralib.get_cv2_stream()
+
+    async def main():
+        token = await login()
+        if token:
+            await asyncio.gather(send_gps_data(token), send_sensor_data(token, 0, 0, 0, 0, 0), stream_video(cam1, cam1, token), listen_for_commands(token))
+        else:
+            print("❌ Neuspešan login, prekidam program.")
+
+    asyncio.run(main())
