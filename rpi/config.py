f451141586ca039c4a267dadd86e904374d0f97d
vekidev <93841364+vekidev@users.noreply.github.com>
2025-05-15 23:08:36 +0200

    glupi smo mi

--- rpi/config.py
+++ rpi/config.py
@@ -1,41 +1,41 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
 import serial
 import RPi.GPIO as GPIO
-from typing import Optional
 
 Temp = '0123456789ABCDEF*'
 
-class Config:
-    FORCE = 17
-    STANDBY = 4
 
-    def __init__(self, baudrate: int = 9600):
-        self.serial: Optional[serial.Serial] = serial.Serial("/dev/ttyS0", baudrate)
+
+class config(object):
+    FORCE  = 17
+    STANDBY= 4
+    def __init__(ser, Baudrate = 9600):
+        ser.serial = serial.Serial("/dev/ttyS0",Baudrate)
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
-        GPIO.setup(self.FORCE, GPIO.IN)
-        GPIO.setup(self.STANDBY, GPIO.OUT)
-        GPIO.output(self.STANDBY, GPIO.HIGH)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(ser.FORCE, GPIO.IN)
+        GPIO.setup(ser.STANDBY, GPIO.OUT)
+        GPIO.output(ser.STANDBY, GPIO.HIGH)
+        
+    def Uart_SendByte(ser, value): 
+        ser.serial.write(value) 
+        
+    def Uart_SendString(ser, value): 
+        ser.serial.write(value)
+  
 
-    def uart_send_byte(self, value: bytes) -> None:
-        if self.serial:
-            self.serial.write(value)
+    def Uart_ReceiveByte(ser): 
+        return ser.serial.read(1)
 
-    def uart_send_string(self, value: str) -> None:
-        if self.serial:
-            self.serial.write(value.encode())
+    def Uart_ReceiveString(ser, value): 
+        data = ser.serial.read(value)
+        return data
+        
+    def Uart_Set_Baudrate(ser, Baudrate):
+         ser.serial = serial.Serial("/dev/ttyS0",Baudrate)
 
-    def uart_receive_byte(self) -> bytes:
-        if self.serial:
-            return self.serial.read(1)
-        return b''
-
-    def uart_receive_string(self, length: int) -> str:
-        if self.serial:
-            return self.serial.read(length).decode(errors='ignore')
-        return ""
-
-    def uart_set_baudrate(self, baudrate: int) -> None:
-        if self.serial:
-            self.serial.baudrate = baudrate
+    
+    
+    
